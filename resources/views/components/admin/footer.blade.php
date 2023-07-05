1b841d4fea6ae99c9a98afccc8de7746da2c174d
Umidjon017 <81345606+Umidjon017@users.noreply.github.com>
2023-07-05 14:18:19 +0500

    first commit

---
+++ resources/views/components/admin/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        <a href="/" target="_blank">{{ __("Project-Name") }}</a>
+    </div>
+
+    <div class="footer-right">
+    </div>
+</footer>
